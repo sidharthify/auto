e62d0ddde9464a978726d63527d239bb41aee17d
sidharthify <wednisegit@gmail.com>
2025-07-10 19:29:06 +0530

    auto.sh: Refactor for building with make
    * Not the full script, to be expanded later.
    Signed-off-by: sidharthify <wednisegit@gmail.com>

--- auto.sh
+++ auto.sh
@@ -1,27 +1,9 @@
 #!/usr/bin/env bash
 
-DIST_DIR="/home/sidharthify/kernel/out/kernel_aarch64/dist"
-KERNEL_BUILD_DIR="/home/sidharthify/kernel/"
+KERNEL_BUILD_DIR="/home/sidharthify/kernel/aosp"
+LLVM_DIR="/home/sidharthify/kernel/prebuilts/clang/host/linux-x86/clang-r487747c/bin/"
 
 cd "${KERNEL_BUILD_DIR}"
 
-tools/bazel run //aosp:kernel_aarch64_dist
-
-cd "${DIST_DIR}"
-zip -r9 kernel.zip \
-    vendor_kernel_boot.img \
-    vendor_dlkm.img \
-    system_dlkm.img \
-    boot.img
-
-echo "zipped images"
-
-curl -s -o u.sh https://raw.githubusercontent.com/Sushrut1101/GoFile-Upload/master/upload.sh
-chmod +x u.sh
-
-bash u.sh kernel.zip
-
-# clean up
-rm -f u.sh kernel.zip
-echo "cleaned up u.sh and kernel.zip"
-
+ARCH=arm64 make LLVM="${LLVM_DIR}" O=out gki_defconfig
+ARCH=arm64 make -j$(nproc) LLVM="${LLVM_DIR}" O=out Image.lz4
